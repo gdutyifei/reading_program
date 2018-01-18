ed9794af92cc19f0b2fc28435c9bc493ba6cdc66
罗奕斐 <gdutyifei@163.com>
2018-01-18 17:59:41 +0800

    创建活动改成form表单提交

--- page/view/create/create.js
+++ page/view/create/create.js
@@ -74,5 +74,11 @@ Page({
   },
   checkboxChange: function (e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
   }
 })
