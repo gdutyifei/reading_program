f51a0fff73baf7c72db32f3ee59f7c1a6ac6d36f
罗奕斐 <gdutyifei@163.com>
2018-02-02 16:25:53 +0800

    首页数据填充

--- page/view/index/index.js
+++ page/view/index/index.js
@@ -1,23 +1,58 @@
 // page/view/index/index.js
 var req = require('../../../util/request.js');
 var config = require('../../../config.js');
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    activityInfo: {},
+    bookInfo: [],
+    openid: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var self = this;
+    app.getUserInfo(function (e) {
+      console.log(e);
+      self.setData({
+        openid: JSON.parse(e).openid
+      })
+    });
     var host = config.host;
     console.log(config.host);
-    req.getRequest(host + "/book/getBookList", {}, "POST", function(res) {
-
+    req.getRequest(host + "/api/getIndexInformation", {}, "POST", function(res) {
+      var data = res.data;
+      console.log(res.data);
+      var bookInfo = data.bookInfo;
+      for(var i in bookInfo) {
+        console.log(bookInfo[i][0]);
+        if (bookInfo[i][0].participates.length > 0) {
+          var userInfos = bookInfo[i][0].participates[0].user_info;
+          bookInfo[i].participates = userInfos;
+          for (var j in userInfos) {
+            if(userInfos[j].openid == self.openid) {
+            
+            }
+          }
+        }
+        
+      }
+      
+      var json = {
+        id: data.id,
+        period: data.period,
+        activityDate: data.activity_date
+      };
+      self.setData({
+        activityInfo: json,
+        bookInfo: bookInfo
+      });
     }, function(err) {
 
     });
