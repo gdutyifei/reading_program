f46f21d4ad03a2c7b63da3276fb319f21b9040ee
罗奕斐 <gdutyifei@163.com>
2018-02-05 15:51:57 +0800

    首页接口修改

--- page/view/index/index.js
+++ page/view/index/index.js
@@ -8,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    activityInfo: {},
+    activityInfo: "",
     bookInfo: [],
     openid: ""
   },
@@ -19,40 +19,50 @@ Page({
   onLoad: function (options) {
     var self = this;
     app.getUserInfo(function (e) {
-      console.log(e);
-      self.setData({
-        openid: JSON.parse(e).openid
-      })
+      
     });
     var host = config.host;
-    console.log(config.host);
+    
     req.getRequest(host + "/api/getIndexInformation", {}, "POST", function(res) {
       var data = res.data;
       console.log(res.data);
-      var bookInfo = data.bookInfo;
-      for(var i in bookInfo) {
-        console.log(bookInfo[i][0]);
-        if (bookInfo[i][0].participates.length > 0) {
-          var userInfos = bookInfo[i][0].participates[0].user_info;
-          bookInfo[i].participates = userInfos;
-          for (var j in userInfos) {
-            if(userInfos[j].openid == self.openid) {
-            
+      var code = data.code;
+      if(code == '999') {
+        console.log("今天没有活动");
+      } else {
+        var bookInfo = data.bookInfo;
+        for (var i in bookInfo) {
+          console.log(bookInfo[i][0].participates);
+          console.log(bookInfo[i][0].participates.length);  
+          if (bookInfo[i][0].participates.length > 0) {
+            var userInfos = bookInfo[i][0].participates;
+            console.log(userInfos);
+            for (var j in userInfos) {
+              if (userInfos[j].openid == self.openid) {
+                // 本人已参加了
+                userInfos[j].isJoin = 'true';
+              } else {
+                userInfos[j].isJoin = 'false';
+              }
             }
+            bookInfo[i].participates = userInfos;
           }
+
         }
-        
+
+        var json = {
+          id: data.id,
+          period: data.period,
+          activityDate: data.activity_date
+        };
+        console.log(111);
+        console.log(json);
+        self.setData({
+          activityInfo: json,
+          bookInfo: bookInfo
+        });
       }
       
-      var json = {
-        id: data.id,
-        period: data.period,
-        activityDate: data.activity_date
-      };
-      self.setData({
-        activityInfo: json,
-        bookInfo: bookInfo
-      });
     }, function(err) {
 
     });
@@ -105,5 +115,24 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  join: function(e) {
+    var self = this;
+    var userInfo = this.userInfo;
+    var bookId = e.currentTarget.dataset.id;
+    var activityId = this.data.activityInfo.id;
+    var openid = app.globalData.openid;
+    var data = {};
+    data.bookId = bookId;
+    data.activityId = activityId;
+    data.openId = openid;
+    var host = config.host;
+    req.getRequest(host + "/participation/join", data, "GET", function (res) {
+        console.log(res);
+        var result = res.data;
+        if(result.code == '200') {
+          // 成功了
+        }
+    })
   }
 })
